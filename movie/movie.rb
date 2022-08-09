26a7a34a7627e4e2d5721931ffb0c2e77dd966ae
abiodunraheem <raheemabiodun23@gmail.com>
2022-08-09 13:40:06 +0100

    Create movie class

---
+++ movie/movie.rb
@@ -0,0 +1,14 @@
+require_relative '../item'
+
+class Movie < Item
+  attr_accessor :silet
+
+  def initialize(publish_date, silet)
+    super(publish_date)
+    @silet = silet
+  end
+
+  def can_be_archived?
+    super or @silet
+  end
+end
