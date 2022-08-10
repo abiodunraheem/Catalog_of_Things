86f81c259a589b06af260e2fc54391e11fc11b36
abiodunraheem <raheemabiodun23@gmail.com>
2022-08-10 10:50:04 +0100

    Update musicalbum table in schema.sql

--- schema.sql
+++ schema.sql
@@ -16,12 +16,11 @@ CREATE TABLE games (
 );
 
 CREATE TABLE IF NOT EXISTS musicalbum (
-    id INT GENERATED ALWAYS AS IDENTITY,
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     on_spotify BOOLEAN,
     publish_date DATE NOT NULL,
     archived BOOLEAN NOT NULL,
     genre_id INT,
-    PRIMARY KEY(id),
     CONSTRAINT genre_id_fk FOREIGN KEY(genre_id) REFERENCES genre(id)
 );
 
