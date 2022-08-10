742f20fdeef885334ce087ffd9575c2299605482
Pedro Marques <ppmarq1@gmail.com>
2022-08-10 11:00:12 +0100

    Update app.rb

--- app.rb
+++ app.rb
@@ -1,13 +1,15 @@
 require_relative './book'
 require_relative './label'
 require_relative 'data/data_book'
-require_relative 'data/data_album'
+require_relative 'data/handle_data'
 require_relative './music_album'
 require_relative './genre'
 require_relative './game'
 require_relative './author'
 require_relative 'data/game_data'
 require 'date'
+
+# rubocop:disable Metrics
 
 class App
   def initialize
@@ -51,6 +53,97 @@ class App
     puts
     puts
     sleep 0.75
+  end
+
+  def list_albums
+    puts
+    puts 'There are no albums to list. Select (8) to create one.' if @albums.empty?
+
+    @albums.each do |album|
+      puts "ID: #{album.id}: Name: #{album.name} Genre: #{album.genre.name} On_Spotify: #{album.on_spotify}"
+      puts
+    end
+  end
+
+  def list_labels
+    puts
+    puts 'There are no labels to show! Please add a label.' if @all_labels.empty?
+
+    @all_labels.each { |label| puts "Id: \'#{label.id}\', Title: \'#{label.title}\', Color: \'#{label.color}\'" }
+    puts
+    puts
+    sleep 0.75
+  end
+
+  def list_genres
+    puts
+    puts 'There are no genres! You can create one by adding a genre.' if @genres.empty?
+
+    @genres.each_with_index { |genre, index| puts "#{index}: Name: #{genre.name}" }
+    puts
+    puts
+  end
+
+  def create_book
+    puts
+    print 'Publisher: '
+    book_publisher = gets.chomp
+
+    print 'Cover State: '
+    book_cover_state = gets.chomp
+
+    print 'Label Title: '
+    book_label_title = gets.chomp
+
+    print 'Label Color: '
+    book_label_color = gets.chomp
+
+    print 'Publish Date (DD-MM-YYYY): '
+    book_publish_date = gets.chomp
+
+    puts
+
+    label = Label.new book_label_title, book_label_color
+
+    mybook = Book.new book_publisher, book_cover_state, book_publish_date
+
+    @all_books << mybook
+    @all_labels << label
+
+    label.add_item(mybook)
+
+    save_book(book_publisher, book_cover_state, book_label_title, book_label_color, book_publish_date)
+
+    puts 'Book created successfully.✅'
+    puts
+    puts
+    sleep 0.75
+  end
+
+  def create_album
+    puts 'Name: '
+    name = gets.chomp
+
+    puts 'Publish Date (DD-MM-YYYY): '
+    date = Date.parse(gets.chomp)
+
+    puts 'Genre: '
+    genre_name = gets.chomp
+
+    puts 'On Spotify (y/n): '
+    on_spotify = gets.chomp
+
+    album = MusicAlbum.new(date, name, on_spotify)
+    genre = Genre.new(genre_name)
+
+    @albums << album
+    @genres << genre
+
+    genre.add_item(album)
+
+    save_album(date, name, genre_name, on_spotify)
+
+    puts "#{name} has been added to the list. ✅"
   end
 
   def create_game
@@ -99,6 +192,10 @@ class App
   end
 
   def load_preserve_data
+    load_album_genre @albums, @genres
+    load_books_and_labels @all_books, @all_labels
     load_manager @games, @authors
   end
 end
+
+# rubocop:enable Metrics
