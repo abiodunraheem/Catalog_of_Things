fddb6b10c3c66786c81001e0f0bc966ba1bec94d
abiodunraheem <raheemabiodun23@gmail.com>
2022-08-12 13:39:35 +0100

    Update test label_spec

--- spec/label_spec.rb
+++ spec/label_spec.rb
@@ -1,15 +1,15 @@
-# require './spec/label'
-# require '.basic/book'
+require './basic/label'
+require '.basic/book'
 
-# describe Label do
-#   before :each do
-#     @label = Label.new 'True Stories', 'Blue'
-#     @book = Book.new 'Pedro', 'bad', '12-07-2000'
-#   end
+describe Label do
+  before :each do
+    @label = Label.new 'True Stories', 'Blue'
+    @book = Book.new 'Pedro', 'bad', '12-07-2000'
+  end
 
-#   it 'The add_item method should add an book item to the label
-#     and total of books in with the label must be 1' do
-#     @label.add_item(@book)
-#     expect(@label.items.length).to eq 1
-#   end
-# end
+  it 'The add_item method should add an book item to the label
+    and total of books in with the label must be 1' do
+    @label.add_item(@book)
+    expect(@label.items.length).to eq 1
+  end
+end
